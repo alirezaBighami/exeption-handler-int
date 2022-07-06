3c680aa0430c73c781f44eeb1a0bd9c5c77a6c2d
alirezaZzzz <alirezabighami79@gmail.com>
2022-07-06 16:51:56 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+string? input1 = ReadLine();
+string? input2 = ReadLine();
+
+try
+{
+    int a = int.Parse(input1);
+    int b = int.Parse(input2);
+    WriteLine(a / b);
+} catch (FormatException)
+{
+    WriteLine("wrong format !!!!!!!!!!!!!!!!");
+}
+
+
+// or
+
+
+if (int.TryParse(input1, out int c) && int.TryParse(input2, out int d))
+{
+    WriteLine(c / d);
+} else
+{
+    WriteLine("wrong format !!!!!!!!!!!!!!!!");
+}
